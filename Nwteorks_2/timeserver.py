5c66b8f622501e489253b8bf59c9c3bd25cc5632
Nir <Nir@Snow>
2019-01-02 14:57:50 +0200

    networks_2

---
+++ Nwteorks_2/timeserver.py
@@ -0,0 +1,11 @@
+from socket import *
+from time import *
+
+server_socket = socket()
+server_socket.bind(("0.0.0.0", 3777))
+server_socket.listen(5)
+while(1):
+    (client_socket, client_address) = server_socket.accept()
+    time_string = asctime() + "\r\n"
+    client_socket.sendall(bytes(time_string, "utf-8"))
+    client_socket.close()
